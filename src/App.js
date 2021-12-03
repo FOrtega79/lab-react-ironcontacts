d62e5e57bd9dcac474be064a5b0ae592009e6e09
Fer Ortega <fernando.ortega79@gmail.com>
2021-12-03 01:09:38 +0100

    iteration 1 to 3 + some styling

--- src/App.js
+++ src/App.js
@@ -6,10 +6,30 @@ import React, { useState } from 'react';
 function App() {
 
   const [contactList, setContactList ] = useState(contacts.slice(0,5))
+
+  const[remainingContacts, setRemainingContacts] = useState(contacts.slice(5, contacts.length))
+
+  function addRandomContact (){
+    const newContact = remainingContacts[Math.floor(Math.random() * remainingContacts.length)]
   
+  const newRemainingContacts = remainingContacts.filter((contact) => contact.id !== newContact.id)
+
+  
+  setContactList([...contactList, newContact])
+  setRemainingContacts(newRemainingContacts)
+  }
+
   return (
+    
     <div className="App">
     <div className="centerAll">
+<div className="pageTitle">
+  <h1>IronContacts 🦸‍♂️</h1>
+</div>
+<div className = "randomBtn">
+  <button className="addChar" onClick={addRandomContact}>Get Random Char</button>
+</div>
+
      <table>
        <tr>
          <th>Picture</th>
@@ -21,11 +41,14 @@ function App() {
 
     {contactList.map((contact, i)=>(
     <tr key={i}>
+    
       <td><img className="facePic" src={contact.pictureUrl} alt="img" /></td>
-      <td><p>{contact.name}</p></td>
-      <td><p>{(contact.popularity).toFixed(2)}</p></td>
+      <td><p className="name">{contact.name}</p></td>
+      <td><p className="popularity">{(contact.popularity).toFixed(2)}</p></td>
       <td>{(contact.wonOscar===true ? "🏆" : "")}</td>
-      <td>{(contact.wonEmmy===true ? "🏀" : "")}</td>
+      <td><p className="Emmy">{(contact.wonEmmy===true ? "🔱" : "")}</p></td>
+      <td><button className="deleteBtn">🗑</button></td>
+
     </tr>
     ))}
 
